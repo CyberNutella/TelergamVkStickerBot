b44c0d8aa54248da8ec7aed7f42b8986439aabfe
AbelianPolymorphism <wshakespear7@gmail.com>
2017-04-16 23:56:40 +0300

    Added associations data loading and storing.

--- TelergamVkStickerBot/Bots/Bot.cs
+++ TelergamVkStickerBot/Bots/Bot.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -11,8 +13,49 @@ namespace TelergamVkStickerBot.Bots
 //    VkBot vkBot = new VkBot();
     TelergammBot tgBot = new TelergammBot();
 
+    public class Association
+    {
+      public string BethesdaName;
+      public long UniqueId;
+      public long VkId;
+      public long TgId;
+    }
+
+    List<Association> Associations = new List<Association>(); // List for slow search, when checking group name while creating unique id, 
+    Hashtable VkToTg = new Hashtable(); // Hashtables for adamantine-cyber-extra-supreme-mega-gold-ultra-fast Id translations
+    Hashtable TgToVk = new Hashtable();
+
     public Bot()
     {
+      /* 413r7 (ALERT) if you have not this file, code will throw some exceptions at you */
+      /* No time to fix */
+
+      /* Associations file example */
+/*
+test#1337; Vk:02, Tg:39
+test#1477; Vk:59, Tg:30
+cake#1477; Vk:11, Tg:47
+ */
+      string []AssTexts = File.ReadAllLines("Data/Associations.txt", System.Text.Encoding.UTF8);
+
+      foreach (string AssText in AssTexts)
+      {
+        Association Ass = new Association();
+
+        Ass.BethesdaName = AssText.Split('#')[0];
+        string x = AssText.Split('#')[1];
+
+        Ass.UniqueId = Int32.Parse(x.Split(';')[0]);
+        x = x.Split(';')[1];
+
+        Ass.VkId = Int32.Parse(x.Split(',')[0].Split(':')[1]);
+        Ass.TgId = Int32.Parse(x.Split(',')[1].Split(':')[1]);
+
+        Associations.Add(Ass);
+        VkToTg.Add(Ass.VkId, Ass);
+        TgToVk.Add(Ass.TgId, Ass);
+      }
+
 //      vkBot.MessageCallblack = VkMessageCallblack;
       tgBot.MessageCallblack = TgMessageCallblack;
       // и похуй что это не статический метод, // <3 C#
